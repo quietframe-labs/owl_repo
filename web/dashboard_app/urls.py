5780bab6eca65520fe26fbf9130cbde4bb1f2846
Matthew Holmes <riverthimble@gmail.com>
2026-08-31 09:57:54 -0700

    updated index and urls

--- web/dashboard_app/urls.py
+++ web/dashboard_app/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 app_name = 'dashboard_app'
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    path('', views.index, name='index'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('live-view/', views.live_view, name='live'),
     path('recordings/', views.recordings, name='recordings'),
     
--- web/dashboard_app/views.py
+++ web/dashboard_app/views.py
@@ -2,6 +2,9 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def index(request):
+    return render(request, 'dashboard_app/index.html')
+
 @login_required
 def dashboard(request):
     return render(request, 'dashboard_app/dashboard.html')
